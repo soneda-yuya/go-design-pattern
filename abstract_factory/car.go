d385a4da1dc95ae9d168f25aa1c2df42a6215f40
soneda-yuya <soneda@nagisa-inc.jp>
2017-05-05 15:58:50 +0900

    abstract_factory

---
+++ abstract_factory/car.go
@@ -0,0 +1,28 @@
+package abstract_factory
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Car interface {
+	NumDoors() int
+}
+
+
+const (
+	LuxuryCarType = 1
+	FamilyCarType = 2
+)
+
+type CarFactory struct{}
+func (c *CarFactory) Build(v int) (Vehicle, error) {
+	switch v {
+		case LuxuryCarType:
+		return new(LuxuryCar), nil
+		case FamilyCarType:
+		return new(FamilyCar), nil
+		default:
+		return nil, errors.New(fmt.Sprintf("Vehicle of type %d not recognized\n", v))
+	}
+}
